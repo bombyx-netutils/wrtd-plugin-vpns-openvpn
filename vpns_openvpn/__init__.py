3867af00686cc66c92fd3930381574e9c29c5303
fpemud <fpemud@sina.com>
2017-07-10 02:57:07 +0000

    fix

--- vpns_openvpn/__init__.py
+++ vpns_openvpn/__init__.py
@@ -239,7 +239,7 @@ class _VirtualBridge:
     def on_source_remove(self, source_id):
         os.unlink(os.path.join(self.hostsDir, source_id))
 
-    def on_host_add_or_change(self, source_id, ip_data_dict):
+    def on_host_add(self, source_id, ip_data_dict):
         fn = os.path.join(self.hostsDir, source_id)
         itemDict = _Util.dnsmasqHostFileToOrderedDict(fn)
         bChanged = False
@@ -261,6 +261,9 @@ class _VirtualBridge:
         if bChanged:
             _Util.dictToDnsmasqHostFile(itemDict, fn)
             self.dnsmasqProc.send_signal(signal.SIGHUP)
+
+    def on_host_change(self, source_id, ip_data_dict):
+        self.on_host_add(self, source_id, ip_data_dict)
 
     def on_host_remove(self, source_id, ip_list):
         fn = os.path.join(self.hostsDir, source_id)
